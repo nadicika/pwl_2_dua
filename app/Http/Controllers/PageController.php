5e84e1de3b6fb9144558dd3915dd5006fd3bc4a2
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-02-27 20:40:30 +0700

    edit pageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,10 +11,11 @@ class PageController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    /*public function index()
     {
-        echo "Selamat Datang";
+        echo "Selamat Datang di Website Kami";
     }
+    */
 
     public function about()
     {
@@ -26,6 +27,72 @@ class PageController extends Controller
         echo "Halaman Artikel dengan ID "; 
         return ($id);
     }
+
+    public function product()
+    {
+        echo "Halaman Products <br>";
+        echo "
+        <ul>
+            <li>
+                <a href='https://www.educastudio.com/category/marbel-edu-games'>Edu Games</a>
+            </li>
+            <li>
+                <a href='https://www.educastudio.com/category/marbel-and-friends-kids-games'>Kids Games</a>
+            </li>
+            <li>
+                <a href='https://www.educastudio.com/category/riri-story-books'>Story Books</a>
+            </li>
+            <li>
+                <a href='https://www.educastudio.com/category/kolak-kids-songs'>Kids Song</a>
+            </li>
+        </ul>";
+    }
+
+    public function news($param)
+    {
+        echo "Halaman News";
+        echo "<br>";
+        echo "
+        <ul>
+            <li>
+                <a href='https://www.educastudio.com/news'>Berita 1</a>
+            </li>
+            <li>
+                <a href='https://www.educastudio.com/news/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19'>Berita 2</a>
+            </li>
+        </ul>";
+    }
+
+    public function program()
+    {
+        echo "Halaman Program <br>";
+        echo "
+        <ul>
+            <li>
+                <a href='https://www.educastudio.com/program/karir'>Program Karir</a>
+            </li>
+            <li>
+            <a href='https://www.educastudio.com/program/magang'>Program Magang</a>
+            </li>
+            <li>
+            <a href='https://www.educastudio.com/program/kunjungan-industri'>Program Kunjungan Industri</a>
+            </li>
+        </ul>";
+    }
+
+    public function index()
+    {
+        return 'KONTAK KAMI <br>
+        <ul>
+            <li> Telp : 02182972 </li>
+            <li> E-mail : edu-web@gmail.com </li>
+        </ul>
+        <label>Kontak</label> <br>
+            <input placeholder="Masukkan kontak">
+            <button>Submit</button>
+        ';
+    }
+
 
     /**
      * Show the form for creating a new resource.
--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,29 @@ Route::get('/about', [PageController::class, 'about']);
 Route::get('/articles/{id}', [PageController::class, 'articles']);
 */
 
+/*
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/', [AboutController::class, 'index']);
 
 Route::get('//{id}', [ArticleController::class, 'index']);
+*/
+
+Route::get('/', [PageController::class, 'index']);
+
+Route::prefix('product')->group(function () {
+    Route::get('/list', [PageController::class, 'product']);
+});
+
+Route::get('/news/{param}', [PageController::class, 'news']);
+
+Route::prefix('program')->group(function () {
+    Route::get('/list', [PageController::class, 'program']);
+});
+
+Route::get('/about-us', function () {
+    echo "Saya Inthania Nadicika Kurniawan seorang mahasiswa dari Politeknik Negeri Malang. <br>
+    Saya berasal dari Jurusan Teknologi Informasi dengan Program Studi Teknik Informatika.";
+});
+
+Route::resource('index', PageController::class);
