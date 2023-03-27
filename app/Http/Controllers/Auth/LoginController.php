30c314880f0d6874554a372d96d03b11b1081891
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-03-27 11:22:53 +0700

    pertemuan 6

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,15 +27,32 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    protected $redirectTo = '/dashboard';
     /**
      * Create a new controller instance.
      *
      * @return void
      */
+
+    public function showLoginForm()
+    {
+        return view('layouts.login');
+    }
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string|',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HobiController;
 use App\Http\Controllers\HomeController;
@@ -11,6 +12,7 @@ use App\Http\Controllers\KuliahController;
 use App\Http\Controllers\MatkulController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -40,41 +42,49 @@ Route::get('/', [AboutController::class, 'index']);
 Route::get('//{id}', [ArticleController::class, 'index']);
 */
 
+Auth::routes();
+Route::get('/logout', [LoginController::class, 'logout']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/profile', [ProfileController::class, 'index']);
+    Route::get('/kuliah', [KuliahController::class, 'index']);
+    Route::get('/kendaraan', [KendaraanController::class, 'index']);
+    Route::get('/hobi', [HobiController::class, 'index']);
+    Route::get('/keluarga', [KeluargaController::class, 'index']);
+    Route::get('/matkul', [MatkulController::class, 'index']);
+});
+
 Route::get('/', [PageController::class, 'index']);
 
 
 
-Route::get('/news/{param}', [PageController::class, 'news']);
+    Route::get('/news/{param}', [PageController::class, 'news']);
+    
+    Route::prefix('program')->group(function () {
+        Route::get('/', [PageController::class, 'program']);
+    });
+    
+    Route::resource('index', PageController::class);
+    
+    //P3
+    Route::get('/home', [PageController::class, 'home']);
+    
+    Route::prefix('product')->group(function () {
+        Route::get('/', [PageController::class, 'product']);
+    });
+    
+    Route::get('/about-us', [PageController::class, 'aboutus']);
+    
+    Route::resource('contact-us', PageController::class)->only(['index']);
+    
+    Route::get('/prak2', function(){
+        return view('layout.template');
+    });
 
-Route::prefix('program')->group(function () {
-    Route::get('/', [PageController::class, 'program']);
-});
 
-Route::resource('index', PageController::class);
 
-//P3
-Route::get('/home', [PageController::class, 'home']);
 
-Route::prefix('product')->group(function () {
-    Route::get('/', [PageController::class, 'product']);
-});
 
-Route::get('/about-us', [PageController::class, 'aboutus']);
-
-Route::resource('contact-us', PageController::class)->only(['index']);
-
-Route::get('/prak2', function(){
-    return view('layout.template');
-});
-
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::get('/profile', [ProfileController::class, 'index']);
-Route::get('/kuliah', [KuliahController::class, 'index']);
-
-Route::get('/kendaraan', [KendaraanController::class, 'index']);
-
-Route::get('/hobi', [HobiController::class, 'index']);
-
-Route::get('/keluarga', [KeluargaController::class, 'index']);
-
-Route::get('/matkul', [MatkulController::class, 'index']);
